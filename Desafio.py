d8fe44982a108bfba13cd76274199a41ed19b59e
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-03-08 13:02:43 -0300

    desafio

--- Desafio.py
+++ Desafio.py
@@ -1,23 +1,25 @@
 class Paciente:
-    pacientes = {}
+    pacientes = []
 
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
         self.historico = []
-        Paciente.pacientes[nome] = self
+        self.paciente = {'Nome': self.nome, 'Idade': self.idade, 'Historico': self.historico}
+        Paciente.pacientes.append(self.paciente)
+        # print(Paciente.pacientes)
 
-        # print(f"""\n ******PACIENTE CADASTRADO COM SUCESSO***** \n
-        #         Nome: {self.nome} \n
-        #         Idade: {self.idade} \n
-        #         """)
+        print(f"""\n****** PACIENTE CADASTRADO COM SUCESSO *****\n
+                Nome: {self.nome}\n
+                Idade: {self.idade}\n
+                """)
 
     def visualizarConsulta(self):
-        print("""Data da consulta: {self.data} \n
-                Paciente: {self.paciente} \n
-                Médico responsável: {self.medico} \n
-                Regitrado por: {self.responsavel}
-            """)
+            print(f"""Data da consulta: {self.data}\n
+                    Paciente: {self.paciente}\n
+                    Médico responsável: {self.medico}\n
+                    Regitrado por: {self.responsavel}
+                """)
 
 class Funcionario:
     funcionarios = []
@@ -25,63 +27,67 @@ class Funcionario:
     def __init__(self, nome, cargo):
         self.nome = nome
         self.cargo = cargo
-    
-    def agendarConsulta(self, data, paciente_nome, medico):
-        self.data = data
-        self.medico = medico
-        self.responsavel = self.nome
 
-        if paciente_nome in Paciente.pacientes:
-            paciente = Paciente.pacientes[paciente_nome]
-            consulta = (data, paciente.nome, medico, self.responsavel)
-            paciente.historico.append(consulta)
-            print(f"Consulta agendada para {paciente.nome}.")
-        else:
-            print("Paciente não encontrado.")
+    def agendarConsulta(self, data, paciente_nome, medico_nome, diagnostico):
+        responsavel = self.nome
 
+        for paciente in Paciente.pacientes:
+            if paciente['Nome'] == paciente_nome:
+                for funcionario in Funcionario.funcionarios:
+                    if funcionario['Nome'] == medico_nome:
+                        consulta = (data, paciente_nome, medico_nome, diagnostico, responsavel)
+                        paciente['Historico'].append(consulta)
+                        print(Paciente.pacientes)
+                    else:
+                        'Médico não encontrado na base de dados.'
+            else:
+                'Paciente não encontrado na base de dados.'
+                        
+                
 class Atendente(Funcionario):
     def __init__(self, nome, cargo):
         super().__init__(nome, cargo)
-        # print(f"""\n ******ATENDENTE CADASTRADO COM SUCESSO***** \n
-        #         Atendente: {self.nome} \n
-        #         Cargo: {self.cargo} \n
-        #         """)
-  
+        self.atendente = {'Nome': self.nome, 'Cargo': self.cargo}
+        Funcionario.funcionarios.append(self.atendente)
+
+        print(f"""\n****** ATENDENTE CADASTRADO COM SUCESSO *****\n
+                Atendente: {self.nome}\n
+                Cargo: {self.cargo}\n
+                """)
+
 class Medico(Funcionario):
     def __init__(self, nome, cargo, especialidade, crm):
         super().__init__(nome, cargo)
-        self.nome = nome
-        self.cargo = cargo
         self.especialidade = especialidade
         self.crm = crm
-        medico = (nome, cargo, especialidade, crm)
-        Funcionario.funcionarios.append(medico)
-        # print(f"""\n ******MÉDICO CADASTRADO COM SUCESSO***** \n
-        #         Médico: {self.nome} \n
-        #         Cargo: {self.cargo} \n
-        #         Especialidade: {self.especialidade} \n
-        #         CRM: {self.crm}""")
-        
-    def prescreverMedicamento(self, paciente_nome, medicamento):
-        paciente = paciente_nome
+        self.medico = {'Nome': self.nome, 'Cargo': self.cargo, 'Especialidade': self.especialidade, 'CRM': self.crm}
+        Funcionario.funcionarios.append(self.medico)
 
+        print(f"""\n****** MÉDICO CADASTRADO COM SUCESSO *****\n
+                Médico: {self.nome}\n
+                Cargo: {self.cargo}\n
+                Especialidade: {self.especialidade}\n
+                CRM: {self.crm}""")
+
+    # def prescreverMedicamento(self, paciente_nome, medicamento):
+        # paciente = paciente_nome
 
 class Enfermeiro(Funcionario):
     def __init__(self, nome, cargo, coren):
         super().__init__(nome, cargo)
-        self.nome = nome
-        self.cargo = cargo
         self.coren = coren
-        enfermeiro = (nome, cargo, coren)
-        Funcionario.funcionarios.append(enfermeiro)
-        # print(f"""\n ******ENFERMEIRO CADASTRADO COM SUCESSO***** \n
-        #         Enfermeiro: {self.nome} \n
-        #         Cargo: {self.cargo} \n
-        #         COREN: {self.coren}""")
+        self.enfermeiro = {'Nome': self.nome, 'Cargo': self.cargo, 'COREN': self.coren}
+        Funcionario.funcionarios.append(self.enfermeiro)
+        # print(Funcionario.funcionarios)
 
-pac = Paciente('Caroline','26')
-med = Medico('José', 'Médico','Oftalmologista', 12345)
-ate = Atendente('Fernando','Atendente')
+        print(f"""\n****** ENFERMEIRO CADASTRADO COM SUCESSO *****\n
+                Enfermeiro: {self.nome}\n
+                Cargo: {self.cargo}\n
+                COREN: {self.coren}""")
+
+pac = Paciente('Caroline', '26')
+med = Medico('José', 'Médico', 'Oftalmologista', 12345)
+ate = Atendente('Fernando', 'Atendente')
 enf = Enfermeiro('Julia', 'Enfermeira', 54321)
 
-ate.agendarConsulta('06/03/2024','Caroline','José')
+ate.agendarConsulta('06/03/2024', 'Caroline', 'Sintomas gripais, remedio administrado.','José')
